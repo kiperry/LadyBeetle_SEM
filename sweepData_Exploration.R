24f7620c25b43139b87b2f648e14539ae82a19d6
Frances Sivakoff <sivakoff.3@osu.edu>
2021-05-18 23:07:15 -0400

    data exploration of the sweep data (data not added yet) based on A.2 in Zuur et al. (2009)

---
+++ sweepData_Exploration.R
@@ -0,0 +1,123 @@
+#data exploration. Copied from "data_exploration_5.25.17.R" from bumblebee HM landscape paper
+
+library(car)
+library(plyr)
+library(AED) #this must be downloaded via github using the following code:
+#install.packages("remotes")
+#remotes::install_github("romunov/AED")
+
+#import data set
+
+#complete cases
+
+#Outliers 
+data(Loyn)
+dotchart(Loyn$ABUND)
+
+#Collinearity: assessed in three ways 
+#(1) Pairwise scatterplots
+
+#Some functions needed for the scatterplots that need to be run before creating the scatter plot
+
+##################################################################
+panel.cor <- function(x, y, digits=1, prefix="", cex.cor = 6)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r1=cor(x,y,use="pairwise.complete.obs")
+  r <- abs(cor(x, y,use="pairwise.complete.obs"))
+  txt <- format(c(r1, 0.123456789), digits=digits)[1]
+  txt <- paste(prefix, txt, sep="")
+  if(missing(cex.cor)) { cex <- 0.9/strwidth(txt) } else {
+    cex = cex.cor}
+  text(0.5, 0.5, txt, cex = cex * r)
+}
+##################################################################
+
+Mypairs <- function(Z) {
+  MyVarx <- colnames(Z)
+  pairs(Z, labels = MyVarx,
+        cex.labels =  2,
+        lower.panel = function(x, y, digits=2, prefix="", cex.cor = 7) {
+          panel.cor(x, y, digits, prefix, cex.cor)}, 
+        upper.panel =  function(x, y) points(x, y, 
+                                             pch = 16, cex = 0.8, 
+                                             col = gray(0.1)))
+  #print(P)
+}
+
+#change this to the appropriate data set (potentially with data transformations)
+Z <- cbind(Loyn$ABUND, Loyn$AREA, Loyn$LDIST, Loyn$DIST, Loyn$YR.ISOL, Loyn$ALT, Loyn$GRAZE)
+colnames(Z) <- c("Abund", "Area", "LDist","Dist","Year Iso","Alt","Graze")
+
+# pairDF <- data.frame(Cu = log10(HMdata$lCu), Fe = log10(HMdata$lFe), Zn = HMdata$lZn, Ba = HMdata$lBa, Cd = log10(HMdata$lCd+.01), Co = log10(HMdata$lCo+.01), Cr = log10(HMdata$lCr+.01), Li = log10(HMdata$lLi), Ni = log10(HMdata$lNi), Pb = log10(HMdata$lPb+.01), Sb = log10(HMdata$lSb+.01), Si = log10(HMdata$lSi), Sr = log10(HMdata$lSr), Month = as.factor(HMdata$Month), Num.larvae = HMdata$Num.larvae)
+
+
+Mypairs(Z)
+
+#(2) Correlation coefficients and (3) Variance inflation factors (VIF)
+
+#just the explanatory variables
+drops <- "Num.larvae"
+edf <- pairDF[ , !(names(pairDF) %in% drops)]
+
+##################################################################
+#VIF FUNCTION.####
+#To use:  corvif(YourDataFile)
+corvif <- function(dataz) {
+  dataz <- as.data.frame(dataz)
+  #correlation part
+  #cat("Correlations of the variables\n\n")
+  #tmp_cor <- cor(dataz,use="complete.obs")
+  #print(tmp_cor)
+  
+  #vif part
+  form    <- formula(paste("fooy ~ ",paste(strsplit(names(dataz)," "),collapse=" + ")))
+  dataz   <- data.frame(fooy=1,dataz)
+  lm_mod  <- lm(form,dataz)
+  
+  cat("\n\nVariance inflation factors\n\n")
+  print(myvif(lm_mod))
+}
+
+
+#Support function for corvif. Will not be called by the user
+myvif <- function(mod) {
+  v <- vcov(mod)
+  assign <- attributes(model.matrix(mod))$assign
+  if (names(coefficients(mod)[1]) == "(Intercept)") {
+    v <- v[-1, -1]
+    assign <- assign[-1]
+  } else warning("No intercept: vifs may not be sensible.")
+  terms <- labels(terms(mod))
+  n.terms <- length(terms)
+  if (n.terms < 2) stop("The model contains fewer than 2 terms")
+  if (length(assign) > dim(v)[1] ) {
+    diag(tmp_cor)<-0
+    if (any(tmp_cor==1.0)){
+      return("Sample size is too small, 100% collinearity is present")
+    } else {
+      return("Sample size is too small")
+    }
+  }
+  R <- cov2cor(v)
+  detR <- det(R)
+  result <- matrix(0, n.terms, 3)
+  rownames(result) <- terms
+  colnames(result) <- c("GVIF", "Df", "GVIF^(1/2Df)")
+  for (term in 1:n.terms) {
+    subs <- which(assign == term)
+    result[term, 1] <- det(as.matrix(R[subs, subs])) * det(as.matrix(R[-subs, -subs])) / detR
+    result[term, 2] <- length(subs)
+  }
+  if (all(result[, 2] == 1)) {
+    result <- data.frame(GVIF=result[, 1])
+  } else {
+    result[, 3] <- result[, 1]^(1/(2 * result[, 2]))
+  }
+  invisible(result)
+}
+#END VIF FUNCTIONS
+##################################################################
+
+
